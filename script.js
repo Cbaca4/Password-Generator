f813bd9bb322caa34d2b39c4e00b300e3477e551
Cristian <139021004+Cbaca4@users.noreply.github.com>
2023-09-09 23:33:26 -0500

    made changes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
-  
-  var passwordLength = parseInt(prompt("password Length"))
-  if (passwordLength < 8 || passwordLength > 128) {
+
+  // Function created for password length
+  var passwordLength = parseInt(prompt("password Length:"))
+
+  if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
     alert("sorry! this an invalid length, please retry.")
-    return generatePassword ()
+  return generatePassword()
   }
 
+//Variables created for pop up prompts
 var uppercase = confirm("Your password should have an uppercase letter. Click OK to continue.");
 var lowercase = confirm("Your password should have a lowercase letter. Click OK to continue.");
 var symbols = confirm("Your password should have a symbol. Click OK to continue.");
@@ -15,28 +18,37 @@ var numbers = confirm("Your password must have numbers. Click OK to continue.");
 var passwordLength = confirm("Your password must be between 8 and 128 characters. Click OK to continue.");
 
 //Variables for allowed passowrd characters
-var uppercaseABC = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-var lowercaseABC = "abcdefghijklmnopqrstuvwxyz";
-var specialSymbols = "!@#$%^&*()?.</>|=+:;,[-_]";
-var numeric = "0123456789";
-var multiSelect =[];
+var uppercaseABC = ['A','B','C','D','E','F','G','H','I','J','K','L','M','O','N','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercaseABC = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','n','p','q','r','s','t','u','v','w','x','y','z'];
+var specialSymbols = ['!','@','#','$','%','^','&','*','(',')','?','.','<','>','|','=','+',':',';',',','[','-','_',']'];
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
 
-if (uppercaseABC === false && specialSymbols=== false && numberic === false) {
- alert("Your password does not meet the password criteria.");
+if (!uppercase || !lowercase || !specialSymbols || !numeric) {
+ alert("Your password does not meet the password criteria.")
 };
 
-if (lowercaseABC) {multiSelect += lowercase;}
-if (uppercaseABC) {multiSelect += uppercase;}
-if (numeric) {multiSelect += numbers;}
-if (specialSymbols) {multiSelect += symbols;}
+var possibleCharacters = [];
 
-var finalPassword = ""
-for (var i = 0; i <passwordLength; i++) {
-  var rng =[Math.floor(Math.random() * multiSelect)];
-  finalPassword = finalPassword + multiSelect[rng];
+if (uppercase) {
+  possibleCharacters = possibleCharacters.concat(uppercaseABC);
+}
+if (lowercase) {
+  possibleCharacters = possibleCharacters.concat(lowercaseABC);
+}
+if (symbols) {
+  possibleCharacters = possibleCharacters.concat(specialSymbols);
+}
+if (numbers) {
+  possibleCharacters = possibleCharacters.concat(numeric);
+}
+
+var finalPassword = "";
+for (var i =0; i <passwordLength; i++){
+  var  rng = Math.floor(Math.random() * possibleCharacters.length);
+  finalPassword = finalPassword + possibleCharacters[rng];
 }
 return finalPassword;
-};
+}
 
 // Write password to the #password input
 function writePassword() {
